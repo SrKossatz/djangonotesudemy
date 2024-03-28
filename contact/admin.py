8c0b5c69904ed691ef14f8dd8710ddcff8bcc443
Átylla Kossatz <162571377+CrunchKossatz@users.noreply.github.com>
2024-03-28 17:53:54 +0000

    create models and admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'created_date')
+    search_fields = ('first_name', 'last_name', 'email', 'created_date')
+    ordering = ('last_name', 'first_name')
+    list_per_page = 15
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Cateory(models.Model):
+    name = models.CharField(max_length=100)
+    show = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(null = True, blank = True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(upload_to='picture/%Y/%m/', blank=True)
+    category = models.ForeignKey(Cateory, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
