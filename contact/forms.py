2b5fbcd517d401d82ba400ef23ff5604ed74e4db
Kossatz Crunch <estagiario_1@crunchequation.com>
2024-04-02 15:45:28 +0100

    create field image

--- contact/forms.py
+++ contact/forms.py
@@ -4,13 +4,15 @@ from .models import Contact
 
 class ContactForm(forms.ModelForm):
   
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    
+  picture = forms.ImageField(
+    widget=forms.FileInput(
+      attrs={'accept': 'image/*',}
+    )
+  )
   
   class Meta:
     model = Contact
-    fields = ['first_name', 'last_name', 'phone', 'email', 'description', 'category', ]
+    fields = ['first_name', 'last_name', 'phone', 'email', 'description', 'category', 'picture', ]
     
     widgets = {
       'first_name': forms.TextInput(
