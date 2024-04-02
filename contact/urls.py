0b85e7b88115885b70b84d059aac84c6123266b8
Kossatz Crunch <estagiario_1@crunchequation.com>
2024-04-02 17:58:53 +0100

    created user

--- contact/urls.py
+++ contact/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
     path('contact/<int:contact_id>/update/', views.update, name='update'),
     path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
     
+    # user CRUD
+    path('user/create', views.register, name='register'),
+    
 ]
---
+++ contact/views/user_forms.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from contact.forms import RegisterForm
+from django.contrib import messages
+from django.shortcuts import redirect
+
+
+
+def register(request):
+  form = RegisterForm()
+
+  if request.method == 'POST':
+    form = RegisterForm(request.POST)
+
+    if form.is_valid():
+      form.save()
+      messages.success(request, 'Usuário cadastrado com sucesso')
+      return redirect('contact:index')
+  return render(
+    request,
+    'contact/register.html',
+    {
+      'form': form
+    }
+  )
