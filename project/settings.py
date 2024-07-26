6fdfd9e43e9fd1c5b366ffe626d723aa0d7858dd
Kossatz <atyllakossatz@gmail.com>
2024-07-26 17:31:02 +0100

    change conf to deploy

--- project/settings.py
+++ project/settings.py
@@ -137,6 +137,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 try:
-    from project.local_settings import *
+    from project.local_settings import * # type: ignore
 except ImportError:
     ...
